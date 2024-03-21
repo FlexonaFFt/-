eda9256c6998dd1ae518f679a84c6c3807e7321a
FlexonaFFt <flexonafft@bk.ru>
2024-03-21 20:01:10 +0300

    ord hometask load

--- Codes/Programming/HomeWork/home_task_4.py
+++ Codes/Programming/HomeWork/home_task_4.py
@@ -1,3 +1,16 @@
-my_dict = {'a': 10, 'b': 5, 'c': 20, 'd': 15}
-max_key = max(my_dict, key=lambda k: ord(k))
-print(f'Ключ с максимальным значением: {max_key}')
+def compare_characters(char1, char2):
+    ord_char1 = ord(char1)
+    ord_char2 = ord(char2)
+    
+    if ord_char1 > ord_char2:
+        return f"{char1} ({ord_char1}) больше {char2} ({ord_char2})"
+    elif ord_char1 < ord_char2:
+        return f"{char1} ({ord_char1}) меньше {char2} ({ord_char2})"
+    else:
+        return f"{char1} ({ord_char1}) равен {char2} ({ord_char2})"
+
+# Пример использования
+symbol1 = 'h'
+symbol2 = 'р'
+result = compare_characters(symbol1, symbol2)
+print(result)
