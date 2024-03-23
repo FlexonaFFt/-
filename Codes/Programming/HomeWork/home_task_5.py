e2460af6467a5d84632a8810c3beff50863eacca
FlexonaFFt <flexonafft@bk.ru>
2024-03-23 14:29:43 +0300

    update__ home_task_5

--- Codes/Programming/HomeWork/home_task_5.py
+++ Codes/Programming/HomeWork/home_task_5.py
@@ -8,15 +8,34 @@ class_mates = [{'name': "Игорь", 'height': "193"},
                {'name': "Данила", 'height': "176"},
                {'name': "Данил Никольский", 'height': "175"}]
 
-def sredniy_rost(class_mates):
-    publick_height = 0
-    for mate in class_mates:
-        publick_height += int(mate['height'])
-    sred = publick_height / len(class_mates)
-    return sred
+zadacha = int(input('Введите номер варианта [1, 2]: '))
 
-answer = sredniy_rost(class_mates)
-print("Средний рост в группе: {:.2f} см".format(answer))
+if zadacha == 1:
+    def sredniy_rost(class_mates):
+        publick_height = 0
+        for mate in class_mates:
+            publick_height += int(mate['height'])
+        sred = publick_height / len(class_mates)
+        return sred
 
-# проверка
-#print((193 + 194 + 188 + 176 + 183 + 175 + 175 + 176 + 175) / 9)
+    answer = sredniy_rost(class_mates)
+    print("Средний рост в группе: {:.2f} см".format(answer))
+
+    # проверка
+    #print((193 + 194 + 188 + 176 + 183 + 175 + 175 + 176 + 175) / 9)
+
+if zadacha == 2:
+    class Group:
+        def __init__(self, class_mates):
+            self.class_mates = class_mates
+
+        def sredniy_rost(self):
+            publick_height = 0
+            for mate in self.class_mates:
+                publick_height += int(mate['height'])
+            sred = publick_height / len(class_mates)
+            return sred
+
+    group = Group(class_mates)
+    answer = group.sredniy_rost()
+    print("Средний рост в группе: {:.2f} см".format(answer))
